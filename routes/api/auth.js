e1345054b41f28471a386d68254dd6f4cab0d280
gryphbecrazeh <gryphbecrazeh@gmail.com>
2019-10-21 10:27:43 -0400

    updated user and auth routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,7 +8,9 @@ const User = require("../../models/User");
 // @route POST api/auth
 // @desc Login to server
 // @access PUBLIC
-router.post("/id:", (req, res) => {
+router.post("/", (req, res) => {
 	let { body } = req;
-	User.find(item => item.userName);
+	User.find(item => item.userName === body.userName)
+		.then(console.log("user found"))
+		.catch(err => console.log("error: ", err));
 });
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,3 +4,22 @@ const router = express.Router();
 
 // Import user model
 const User = require("../../models/User");
+
+// @route GET api/user
+// @desc Get users from the server
+// @access PUBLIC
+router.get("/", (req, res) => {
+	User.find()
+		.then(console.log("users found"))
+		.catch(err => console.log("error: ", err));
+});
+
+// @route POST api/user
+// @desc Add user to server
+// @access PUBLIC
+router.post("/", (req, res) => {
+	let { body } = req;
+	User.find(item => item.userName === body.userName)
+		.then(console.log("user available"))
+		.catch(err => console.log("error: ", err));
+});
