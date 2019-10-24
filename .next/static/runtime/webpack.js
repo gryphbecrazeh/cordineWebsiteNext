3108ee56f1b80b4050ae0ea516c33d62f345ba3d
gryphbecrazeh <gryphbecrazeh@gmail.com>
2019-10-24 13:34:50 -0400

    updated design, implemented more variables

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "76fad8fd446126efaae5";
+/******/ 	var hotCurrentHash = "f93982bff5dd9d723e5a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
