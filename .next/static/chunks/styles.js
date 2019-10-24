f53216d95de0e42f377de7b8261013e70b87d9eb
gryphbecrazeh <gryphbecrazeh@gmail.com>
2019-10-24 13:38:18 -0400

    merged issues to working

--- .next/static/chunks/styles.js
+++ .next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1571935992509");
+            injectCss(link, link.href.split("?")[0] + "?unix=1571938583195");
           });
       }
     }
@@ -66,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1571938413421");
+            injectCss(link, link.href.split("?")[0] + "?unix=1571938582856");
           });
       }
     }
--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f93982bff5dd9d723e5a";
+/******/ 	var hotCurrentHash = "828fbc7104159d865116";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
